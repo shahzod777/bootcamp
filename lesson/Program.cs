58148ded074bc9f30bc789cf07d94629c199245b
shahzod777 <xurramovshaxzod777@gmail.com>
2021-10-12 13:19:30 +0500

    added

---
+++ lesson/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
