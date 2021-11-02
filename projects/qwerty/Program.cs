6875877ca3c7a4ae3fdb978e51747cef7c452a6e
shahzod777 <xurramovshaxzod777@gmail.com>
2021-11-02 14:57:44 +0500

    add

---
+++ projects/qwerty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace qwerty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
